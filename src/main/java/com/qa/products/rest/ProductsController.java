854c1f2e19286c36dd5f78edc2e202d576fdc5aa
jingkeduan <jingke.d@hotmail.com>
2022-03-17 23:25:51 +0000

    all methods for crud requests created

--- src/main/java/com/qa/products/rest/ProductsController.java
+++ src/main/java/com/qa/products/rest/ProductsController.java
@@ -1,10 +1,61 @@
 package com.qa.products.rest;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.products.domain.Products;
+import com.qa.products.service.ProductsServiceDB;
 
 
 @RestController
 public class ProductsController{
+	private ProductsServiceDB service;
+
+	public ProductsController(ProductsServiceDB service) {
+		super();
+		this.service=service;
+	}
 	
+	@PostMapping("/create")
+	public ResponseEntity<Products> createProduct(@RequestBody Products a){
+		return new ResponseEntity<Products>(this.service.create(a),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/readAll")
+	public List<Products> readAllProducts(){
+		return this.service.readAll();
+	}
+	
+	@GetMapping("/readById/{id}")
+	public Products readById(@PathVariable Long id) {
+		return this.service.readById(id);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Products> update(@PathVariable Long id, @RequestBody Products a) {
+		return new ResponseEntity<Products>(this.service.update(id,a),HttpStatus.FOUND); 
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<Boolean> delete(@PathParam("id") Long id){
+		return new ResponseEntity<Boolean>(this.service.delete(id),HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("/remove")
+	public ResponseEntity<Products> remove(@PathParam("id") Long id) {
+		return new ResponseEntity<Products>(this.service.remove(id),HttpStatus.ACCEPTED);
+	}
+
 	
 }
--- src/main/java/com/qa/products/service/ProductsServiceDB.java
+++ src/main/java/com/qa/products/service/ProductsServiceDB.java
@@ -2,51 +2,75 @@ package com.qa.products.service;
 
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.qa.products.domain.Products;
+import com.qa.products.repo.ProductsRepo;
 
 
 @Service
 public class ProductsServiceDB implements ProductsInterface {
 
+private ProductsRepo repo;
+	
+	public ProductsServiceDB(ProductsRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
 	@Override
 	public Products create(Products x) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.save(x);
+
 	}
 
 	@Override
 	public List<Products> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.findAll();
 	}
 
 	@Override
 	public Products readById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repo.findById(id).orElse(null);
 	}
 
 	@Override
 	public Products update(Long id, Products y) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Products> a = this.repo.findById(id);
+		Products newItem = a.get();
+		newItem.setCategory(y.getCategory());
+		newItem.setId(id);
+		newItem.setName(y.getName());
+		newItem.setPrice(y.getPrice());
+		newItem.setQuantity(y.getQuantity());
+		return this.repo.save(newItem);
+
 	}
 
 	@Override
 	public Boolean delete(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Products> toDelete = this.repo.findById(id);
+		if (toDelete.isPresent()) {
+			this.repo.deleteById(id);
+			if (!this.repo.existsById(id)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return true; //to indicate possibility of an object already been deleted before this operation
+
 	}
 
 	@Override
 	public Products remove(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Products> toDelete = this.repo.findById(id);
+		this.repo.deleteById(id);
+		return toDelete.orElse(null);
 	}
+
 
 
 
